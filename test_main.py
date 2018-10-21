ff3fa412ecbb283182977c62087f24c90c9a555d
OlegMoiseev <olegmoiseev2698@gmail.com>
2018-10-21 17:22:57 +0300

    Updated tests

--- test_main.py
+++ test_main.py
@@ -4,8 +4,8 @@ import pytest
 
 
 def test_add():
-	assert(add(1, 2) == 3)
+	assert(main.add(1, 2) == 3)
 
 
 def test_get_zero_matrix():
-	assert(get_zero_matrix(3) == np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
+	assert(main.get_zero_matrix(3) == np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
